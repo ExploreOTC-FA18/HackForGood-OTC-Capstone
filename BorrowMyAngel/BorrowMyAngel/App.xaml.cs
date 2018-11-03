b0f4b1b4787d1ea5bdaac4f0dcc695d547ce4f09
Nicholas Ingram <ingram.nicholas.a@gmail.com>
2018-11-03 04:38:24 -0500

    Started work on the main page

--- BorrowMyAngel/BorrowMyAngel/App.xaml.cs
+++ BorrowMyAngel/BorrowMyAngel/App.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using BorrowMyAngel.View;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
---
+++ BorrowMyAngel/BorrowMyAngel/View/MainPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+
+namespace BorrowMyAngel.View
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            //When the page loads let's find out what the size of the banner
+            //  should be. We want it to always be 1/16th of the current screen
+            //  size.
+            banner.HeightRequest = DeviceDisplay.ScreenMetrics.Height / 16;
+        }
+    }
+}
