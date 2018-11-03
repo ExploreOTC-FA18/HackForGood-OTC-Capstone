e784fcf7a2bf52002022bf0443bf156d3b991375
Shiro <liggettt@otc.edu>
2018-11-03 14:43:39 -0500

    Adds the connection button

---
+++ BorrowMyAngel/BorrowMyAngel/View/ConnectionScreen.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace BorrowMyAngel.View
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ConnectionScreen : ContentPage
+    {
+        public ConnectionScreen()
+        {
+            InitializeComponent();
+            var halfOfScreenWidth = (int)(DeviceDisplay.ScreenMetrics.Width / DeviceDisplay.ScreenMetrics.Density) / 2;
+            btnWaiting.CornerRadius = halfOfScreenWidth;
+            btnWaiting.HeightRequest = halfOfScreenWidth;
+            btnWaiting.WidthRequest = halfOfScreenWidth;
+
+        }
+    }
+}
